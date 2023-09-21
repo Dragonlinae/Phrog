581cbb93d9cb87b7ffebefd16e45c79f6df37788
Joshua <joshuahsu108@gmail.com>
2023-09-20 21:55:03 -0700

    Add blueprint

--- Source/Phrog/Phrog.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Phrog.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Phrog, "Phrog" );
- 
